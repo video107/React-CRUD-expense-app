5dc1f84e7332ba37fdcfd69f3b31daf7cc5f6f25
John Chang <tschangc@gmail.com>
2016-01-07 18:30:27 +0800

    Creating 'RecordForm' component. Adding new records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,4 +3,21 @@ class RecordsController < ApplicationController
     @records = Record.all
   end
 
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save!
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def record_params
+    params.require(:record).permit(:title,:date,:amount)
+  end
+
 end
